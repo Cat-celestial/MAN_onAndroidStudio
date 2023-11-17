f89a669f8e8875e25be0e339cc13b41639e6888d
Cat-celestial <kirillkoten@gmail.com>
2023-11-17 19:31:47 +0200

    make the first par img and theory(without text design)

---
+++ app/src/main/assets/script.js
@@ -0,0 +1,7 @@
+//document.querySelector(".menuButtImg").onclick = function() {
+//    if (document.querySelector(".menu").style.left == "0") {
+//        document.querySelector(".menu").style.left = "-90vw";
+//    } else {
+//        document.querySelector(".menu").style.left = "0";
+//    }
+//}
