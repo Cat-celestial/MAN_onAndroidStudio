4ec7d1bd3b3f7b0d72d9e37e13167350b964faa1
Cat-celestial <kirillkoten@gmail.com>
2023-11-19 23:41:41 +0200

    сделал пример , делаю решаем вместе, я думаю ну нафиг эту кнопку, она еще с сенсором не работает, че делать, осталось 3 дня, и нужно сделать 12 страниц документа, и поменять уже готовые, и доделать эту и еще хотя бы одну тему, ну хоть уже по макетам, надеюсь успею, капец

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,3 +1,20 @@
+let firstAnswText = document.querySelector(".rootsTask1Sotution");
+let showSolutionRootButt = document.querySelector(".showRootSolutios");
+
+function showSoltRootFunc () {
+    firstAnswText.style.opacity = "1";
+};
+
+showSolutionRootButt.addEventListener("touchend", showSoltRootFunc(), false);
+
+
+// let firstAnswTextOpacityState = window.getComputedStyle(firstAnswText, null).getPropertyValue("opacity");
+// if (firstAnswTextOpacityState == "0") {
+//     firstAnswText.style.opacity = "1";
+// } else if (firstAnswTextOpacityState == "1") {
+//     firstAnswText.style.opacity = "0";
+// };
+
 //document.querySelector(".menuButtImg").onclick = function() {
 //    if (document.querySelector(".menu").style.left == "0") {
 //        document.querySelector(".menu").style.left = "-90vw";
