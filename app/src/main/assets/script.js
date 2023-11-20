f06505f61e67974af253aaee0f29204b6c7e9bbb
Cat-celestial <kirillkoten@gmail.com>
2023-11-21 01:55:00 +0200

    кажется доделал корень н-го степеня полностью теперь прогресии, максимум через 3 дня нужно здать, а еще нужно арефм и геом прогресию, написать файл еще на 12 страниц, и изменить существующие, нужно успевать!

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,20 +1,23 @@
-let firstAnswText = document.querySelector(".rootsTask1Sotution");
-let showSolutionRootButt = document.querySelector(".showRootSolutios");
+// let firstAnswText = document.querySelector(".rootsTask1Sotution");
+// let showSolutionRootButt = document.querySelector(".showRootSolutios");
 
-function showSoltRootFunc () {
-    firstAnswText.style.opacity = "1";
-};
-
-showSolutionRootButt.addEventListener("touchend", showSoltRootFunc(), false);
-
-
+// showSolutionRootButt.onclick = function () {
+//     firstAnswText.style.opacity = "1";
+// }
+// showSolutionRootButt.addEventListener("touchend", () => {
+//     firstAnswText.style.opacity = "1";
+// });
+// function showSoltRootFunc () {
+//     firstAnswText.style.opacity = "1";
+// };
+// showSolutionRootButt.addEventListener("touchstart", showSoltRootFunc);
+// document.addEventListener("DOMContentLoaded", showSoltRootFunc);
 // let firstAnswTextOpacityState = window.getComputedStyle(firstAnswText, null).getPropertyValue("opacity");
 // if (firstAnswTextOpacityState == "0") {
 //     firstAnswText.style.opacity = "1";
 // } else if (firstAnswTextOpacityState == "1") {
 //     firstAnswText.style.opacity = "0";
 // };
-
 //document.querySelector(".menuButtImg").onclick = function() {
 //    if (document.querySelector(".menu").style.left == "0") {
 //        document.querySelector(".menu").style.left = "-90vw";
